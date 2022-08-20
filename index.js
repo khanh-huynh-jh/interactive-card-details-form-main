76c5b98cd95a7dfc8520021ade3a6e8d3b82e900
hnvkhanh <92099362+hnvkhanh@users.noreply.github.com>
2022-08-20 13:20:23 +0700

    finish interactive form for desktop screen

--- index.js
+++ index.js
@@ -3,13 +3,26 @@ submit_btn = document.querySelector("button");
 
 submit_btn.addEventListener("click", function(){
     for (var i=0; i < inputs.length; i++){
+        if (invalid_length(inputs[i].id, inputs[i].value.length)){
+            message = document.querySelector(message_id(inputs[i].id));
+            message.innerHTML = "Invalid value"; 
+            message.style.color = "hsl(0, 100%, 66%)";
+            inputs[i].style.setProperty('border-color', 'hsl(0, 100%, 66%)');
+        }
         if (inputs[i].value == ""){
             message = document.querySelector(message_id(inputs[i].id));
             message.innerHTML = "Can't be blank"; 
             message.style.color = "hsl(0, 100%, 66%)";
             inputs[i].style.setProperty('border-color', 'hsl(0, 100%, 66%)');
         }        
-    }        
+    }    
+    
+    if (non_number_monitor(document.querySelector("#card-number-input").value)){
+        message = document.querySelector(message_id('card-number-input'));
+        message.innerHTML = "Wrong format, numbers only";
+        message.style.color = "hsl(0, 100%, 66%)";
+        document.querySelector("#card-number-input").style.setProperty('border-color', 'hsl(0, 100%, 66%)');
+    }   
 });
 
 for (var i = 0; i < inputs.length; i++){
@@ -28,6 +41,8 @@ document.querySelector('#card-number-input').addEventListener('input', function(
         newval = newval.concat(val[i]);
     }    
     this.value = newval;
+
+    
 });
 
 document.querySelector('#year-expired-input').addEventListener('input', function(e){              
@@ -98,4 +113,35 @@ function message_invalid_value(id, data, element, value = ''){
         }
     }    
 }
+
+function non_number_monitor(card_number){
+    var n = card_number.length;
+
+    for (var i = 0; i < n; i++){
+        char_code = card_number.charCodeAt(i);
+        if (char_code != 32){
+            if (char_code < 48 || char_code > 57){
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+function invalid_length(id, length){
+    if (length == 0){
+        return false;
+    }
+    switch(id) {
+        case "cvc-input":
+            return length < 3;            
+        case "card-number-input":
+            return length < 19;                                
+        default:
+            return length < 2;
+      }
+}
+
+
 
